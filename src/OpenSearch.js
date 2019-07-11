a663252985b951c7cb37fd8a901aa0405e3d0e4d
Cian <cian.durrigan@gmail.com>
2019-07-11 14:55:40 +0100

    wrap the opensearch button in the Link component

--- src/OpenSearch.js
+++ src/OpenSearch.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class OpenSearch extends Component {
   render() {
     return (
       <div className="open-search">
-        <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+        <Link to='/search' className='open-search-link'>
+          <button>Add a Book</button>
+        </Link>
       </div>
     )
   }
